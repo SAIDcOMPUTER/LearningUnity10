3e6a7b173dd04adaa4646789bb81bbc68735b705
WaOLi <meauxreauxt@outlook.com>
2021-08-29 17:29:37 +0300

    Initial commit

---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    public LineRenderer sling;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
